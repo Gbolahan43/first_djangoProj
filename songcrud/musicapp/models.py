c9b99d530660fb65bc8b2ddc53a0d7ea6f73fb14
Gbolahan43 <gbolahanbasit43@gmail.com>
2022-10-29 08:47:56 +0100

    new commit

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,23 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from datetime import datetime
+
+from traitlets import default
 
 # Create your models here.
+class Artiste (models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    age = models.IntegerField()
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length = 400)
+    date_released = models.DateField(default.datetime.today)
+    likes = models.IntegerField()
+    artiste_id = models.CharField(max_length = 50)
+
+class Lyric(models.Model):
+    Song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length = 400)
+    song_id = models.CharField(max_length = 50)
+
